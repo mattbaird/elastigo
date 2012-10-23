add9d4a5f0877afa9275a8b2061c892abddcbb77
Matthew Baird <mattbaird@gmail.com>
2012-10-22 17:40:20 -0700

    use omitifempty

--- api/baseResponse.go
+++ api/baseResponse.go
@@ -2,13 +2,13 @@ package api
 
 type BaseResponse struct {
 	Ok      bool        `json:"ok"`
-	Index   string      `json:"_index"`
-	Type    string      `json:"_type"`
-	Id      string      `json:"_id"`
-	Source  interface{} `json:"_source"` // depends on the schema you've defined
-	Version int         `json:"_version"`
-	Found   bool        `json:"found"`
-	Exists  bool        `json:"exists"`
+	Index   string      `json:"_index,omitifempty"`
+	Type    string      `json:"_type,omitifempty"`
+	Id      string      `json:"_id,omitifempty"`
+	Source  interface{} `json:"_source,omitifempty"` // depends on the schema you've defined
+	Version int         `json:"_version,omitifempty"`
+	Found   bool        `json:"found,omitifempty"`
+	Exists  bool        `json:"exists,omitifempty"`
 }
 
 func Pretty(pretty bool) string {
@@ -18,5 +18,3 @@ func Pretty(pretty bool) string {
 	}
 	return prettyString
 }
-
-
