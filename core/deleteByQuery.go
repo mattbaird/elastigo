5394d49cf67fed89aaff7581fec7f9955bf9b5f2
Matthew Baird <mattbaird@gmail.com>
2012-10-22 17:07:18 -0700

    initial commit delete by query and mget

--- core/deleteByQuery.go
+++ core/deleteByQuery.go
@@ -1,1 +1,58 @@
 package core
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/mattbaird/elastigo/api"
+	"strings"
+)
+
+// The delete by query API allows to delete documents from one or more indices and one or more types based on a query. 
+// The query can either be provided using a simple query string as a parameter, or using the Query DSL defined within 
+// the request body.
+// see: http://www.elasticsearch.org/guide/reference/api/delete-by-query.html
+func DeleteByQuery(pretty bool, indices []string, types []string, query interface{}) (api.BaseResponse, error) {
+	var url string
+	var retval api.BaseResponse
+	if len(indices) > 0 && len(types) > 0 {
+		url = fmt.Sprintf("http://localhost:9200/%s/%s/_query?%s&%s", strings.Join(indices, ","), strings.Join(types, ","), buildQuery, api.Pretty(pretty))
+	} else if len(indices) > 0 {
+		url = fmt.Sprintf("http://localhost:9200/%s/_query?%s&%s", strings.Join(indices, ","), buildQuery, api.Pretty(pretty))
+	}
+	body, err := api.DoCommand("DELETE", url, query)
+	if err != nil {
+		return retval, err
+	}
+	if err == nil {
+		// marshall into json
+		jsonErr := json.Unmarshal([]byte(body), &retval)
+		if jsonErr != nil {
+			return retval, jsonErr
+		}
+	}
+	fmt.Println(body)
+	return retval, err
+}
+
+func buildQuery() string {
+	return ""
+}
+
+type DeleteByQueryResponse struct {
+	Status   bool       `json:"ok"`
+	Indicies []Indicies `json:"_indices"`
+}
+
+type Indicies struct {
+	Name Shards
+}
+
+type IndexStatus struct {
+	Shards ShardStatus `json:"_shards"`
+}
+
+type ShardStatus struct {
+	Total      int `json:"total"`
+	Successful int `json:"successful"`
+	Failed     int `json:"failed"`
+}
--- core/mget.go
+++ core/mget.go
@@ -1,1 +1,52 @@
 package core
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/mattbaird/elastigo/api"
+)
+
+// Multi GET API allows to get multiple documents based on an index, type (optional) and id (and possibly routing).
+// The response includes a docs array with all the fetched documents, each element similar in structure to a document 
+// provided by the get API.
+// see http://www.elasticsearch.org/guide/reference/api/multi-get.html
+func MGet(pretty bool, index string, _type string, mgetRequest MGetRequestContainer) (MGetResponseContainer, error) {
+	var url string
+	var retval MGetResponseContainer
+	if len(index) <= 0 {
+		url = fmt.Sprintf("/_mget?%s", api.Pretty(pretty))
+	}
+	if len(_type) > 0 && len(index) > 0 {
+		url = fmt.Sprintf("/%s/%s/_mget?%s", index, _type, api.Pretty(pretty))
+	} else if len(index) > 0 {
+		url = fmt.Sprintf("/%s/_mget?%s", index, api.Pretty(pretty))
+	}
+	body, err := api.DoCommand("GET", url, nil)
+	if err != nil {
+		return retval, err
+	}
+	if err == nil {
+		// marshall into json
+		jsonErr := json.Unmarshal([]byte(body), &retval)
+		if jsonErr != nil {
+			return retval, jsonErr
+		}
+	}
+	fmt.Println(body)
+	return retval, err
+}
+
+type MGetRequestContainer struct {
+	Docs []MGetRequest `json:"docs"`
+}
+
+type MGetRequest struct {
+	Index  string   `json:"_index"`
+	Type   string   `json:"_type"`
+	ID     string   `json:"_id"`
+	Fields []string `json:"fields,omitifempty"`
+}
+
+type MGetResponseContainer struct {
+	Docs []api.BaseResponse `json:"docs"`
+}
