d1938db8e9a66a32c1d68f8b0067a1bdb84d571d
Matthew Baird <mattbaird@gmail.com>
2013-09-27 15:09:58 -0700

    implemented indices clear cache API

--- indices/clearCache.go
+++ indices/clearCache.go
@@ -8,5 +8,46 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 package indices
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/mattbaird/elastigo/api"
+	"net/url"
+	"strings"
+)
+
+// ClearCache allows to clear either all caches or specific cached associated with one ore more indices.
+// see http://www.elasticsearch.org/guide/reference/api/admin-indices-clearcache/
+func ClearCache(clearId bool, clearBloom bool, indices ...string) (api.ExtendedStatus, error) {
+	var retval api.ExtendedStatus
+	var clearCacheUrl string
+	if len(indices) > 0 {
+		clearCacheUrl = fmt.Sprintf("/%s/_cache/clear", strings.Join(indices, ","))
+
+	} else {
+		clearCacheUrl = fmt.Sprintf("/_cache/clear")
+	}
+	var values url.Values = url.Values{}
+
+	if clearId {
+		values.Add("id", "true")
+	}
+	if clearBloom {
+		values.Add("bloom", "true")
+	}
+	clearCacheUrl += "?" + values.Encode()
+	body, err := api.DoCommand("POST", clearCacheUrl, nil)
+	if err != nil {
+		return retval, err
+	}
+	if err == nil {
+		// marshall into json
+		jsonErr := json.Unmarshal(body, &retval)
+		if jsonErr != nil {
+			return retval, jsonErr
+		}
+	}
+	return retval, err
+}
