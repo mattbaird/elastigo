7df8bf894811d9365e5434aa9ff5dd4a26c8042d
Matthew Baird <mattbaird@gmail.com>
2014-05-21 12:58:04 -0700

    add more options to index call

--- core/index.go
+++ core/index.go
@@ -13,32 +13,49 @@ package core
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/mattbaird/elastigo/api"
+	"log"
+	"net/url"
+	"strconv"
 )
 
 // Index adds or updates a typed JSON document in a specific index, making it searchable, creating an index
 // if it did not exist.
-// if id is omited, op_type 'create' will be pased and http method will default to "POST"
+// if id is omited, op_type 'create' will be passed and http method will default to "POST"
+// _type is optional
 // id is optional
+// parentId is optional
+// version is optional
+// op_type is optional
+// routing is optional
+// timestamp is optional
+// ttl is optional
+// percolate is optional
+// timeout is optional
 // http://www.elasticsearch.org/guide/reference/api/index_.html
 func Index(index string, _type string, id string, args map[string]interface{}, data interface{}) (api.BaseResponse, error) {
-	var retval api.BaseResponse
+	return IndexWithParameters(index, _type, id, "", 0, "", "", "", 0, "", "", false, args, data)
+}
+
+// IndexWithParameters takes all the potential parameters available
+func IndexWithParameters(index string, _type string, id string, parentId string, version int, op_type string,
+	routing string, timestamp string, ttl int, percolate string, timeout string, refresh bool,
+	args map[string]interface{}, data interface{}) (api.BaseResponse, error) {
 	var url string
-
-	if len(id) > 0 {
-		url = fmt.Sprintf("/%s/%s/%s", index, _type, id)
-	} else {
-		url = fmt.Sprintf("/%s/%s", index, _type)
+	var retval api.BaseResponse
+	url, err := GetIndexUrl(index, _type, id, parentId, version, op_type, routing, timestamp, ttl, percolate, timeout, refresh)
+	if err != nil {
+		return retval, err
 	}
-
 	var method string
 	if len(id) == 0 {
 		method = "POST"
 	} else {
 		method = "PUT"
 	}
-
+	log.Printf("about to :%v %v %v", url, args, data)
 	body, err := api.DoCommand(method, url, args, data)
 	if err != nil {
 		return retval, err
@@ -51,4 +68,68 @@ func Index(index string, _type string, id string, args map[string]interface{}, d
 		}
 	}
 	return retval, err
+}
+
+func GetIndexUrl(index string, _type string, id string, parentId string, version int, op_type string,
+	routing string, timestamp string, ttl int, percolate string, timeout string, refresh bool) (retval string, e error) {
+
+	if len(index) == 0 {
+		return "", errors.New("index can not be blank")
+	}
+	var partialURL string
+	var values url.Values = url.Values{}
+	if len(_type) == 0 && len(id) > 0 {
+		e = errors.New("Can't specify id when _type is blank")
+		return
+	}
+	if len(_type) > 0 && len(id) > 0 {
+		partialURL = fmt.Sprintf("/%s/%s/%s", index, _type, id)
+	} else if len(_type) > 0 {
+		partialURL = fmt.Sprintf("/%s/%s", index, _type)
+	} else {
+		partialURL = fmt.Sprintf("/%s", index)
+	}
+	// A child document can be indexed by specifying it’s parent when indexing.
+	if len(parentId) > 0 {
+		values.Add("parent", parentId)
+	}
+	// versions start at 1, so if greater than 0
+	if version > 0 {
+		values.Add("version", strconv.Itoa(version))
+	}
+	if len(op_type) > 0 {
+		if len(id) == 0 {
+			//if id is omited, op_type defaults to 'create'
+			values.Add("op_type", "create")
+		} else {
+			values.Add("op_type", op_type)
+		}
+	}
+	if len(routing) > 0 {
+		values.Add("routing", routing)
+	}
+	// A document can be indexed with a timestamp associated with it.
+	// The timestamp value of a document can be set using the timestamp parameter.
+	if len(timestamp) > 0 {
+		values.Add("timestamp", timestamp)
+	}
+	// A document can be indexed with a ttl (time to live) associated with it. Expired documents
+	// will be expunged automatically.
+	if ttl > 0 {
+		values.Add("ttl", strconv.Itoa(ttl))
+	}
+	if len(percolate) > 0 {
+		values.Add("percolate", percolate)
+	}
+	// example 5m
+	if len(timeout) > 0 {
+		values.Add("timeout", timeout)
+	}
+
+	if refresh {
+		values.Add("refresh", "true")
+	}
+
+	partialURL += "?" + values.Encode()
+	return partialURL, nil
 }
