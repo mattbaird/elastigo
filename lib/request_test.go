6f948380b5dc800a441e2cfce74efc2171cd9242
Chris Vanderschuere <chris@offlinelabs.com>
2015-10-06 00:05:42 -0700

    Fixed comments

--- lib/request_test.go
+++ lib/request_test.go
@@ -37,7 +37,7 @@ func TestQueryString(t *testing.T) {
 	exp = "foo=1"
 	assert.T(t, s == exp && err == nil, fmt.Sprintf("Expected %s, got: %s", exp, s))
 
-	// Test single int64 argument
+	// Test single int32 argument
 	s, err = Escape(map[string]interface{}{"foo": int32(1)})
 	exp = "foo=1"
 	assert.T(t, s == exp && err == nil, fmt.Sprintf("Expected %s, got: %s", exp, s))
@@ -47,6 +47,7 @@ func TestQueryString(t *testing.T) {
 	exp = "foo=3.141592"
 	assert.T(t, s == exp && err == nil, fmt.Sprintf("Expected %s, got: %s", exp, s))
 
+	// Test single float32 argument
 	s, err = Escape(map[string]interface{}{"foo": float32(3.141592)})
 	exp = "foo=3.141592"
 	assert.T(t, s == exp && err == nil, fmt.Sprintf("Expected %s, got: %s", exp, s))
