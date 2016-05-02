b3f7a5828e333e7c9f7db89a7026929ac8366cf4
Shawn Smith <shawnpsmith@gmail.com>
2016-05-02 13:50:53 +0900

    fix typos

--- lib/catnodeinfo.go
+++ lib/catnodeinfo.go
@@ -20,7 +20,7 @@ func newCatNodeInfo(fields []string, indexLine string) (catNode *CatNodeInfo, er
 		return nil, fmt.Errorf("Number of fields (%d) greater than number of stats (%d)", lf, ls)
 	}
 
-	// Populate the apropriate field in CatNodeInfo
+	// Populate the appropriate field in CatNodeInfo
 	for i, field := range fields {
 
 		switch field {
@@ -164,7 +164,7 @@ func newCatNodeInfo(fields []string, indexLine string) (catNode *CatNodeInfo, er
 			catNode.PercTime = split[i]
 		case "percolate.total", "pto", "percolateTotal":
 			catNode.PercTotal = split[i]
-		case "refesh.total", "rto", "refreshTotal":
+		case "refresh.total", "rto", "refreshTotal":
 			catNode.RefreshTotal = split[i]
 		case "refresh.time", "rti", "refreshTime":
 			catNode.RefreshTime = split[i]
